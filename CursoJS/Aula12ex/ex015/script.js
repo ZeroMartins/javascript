4ef548bb16e85e4ab099e04b5043a8494d2ca07a
Aloisio Martins <130004770+ZeroMartins@users.noreply.github.com>
2026-03-15 12:13:36 -0300

    update

---
+++ CursoJS/Aula12ex/ex015/script.js
@@ -0,0 +1,49 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'imagens/bb-menino.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'imagens/jovem-h.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'imagens/homem-adulto.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'imagens/idoso.png')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'imagens/bb-menina.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'imagens/jovem-m.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'imagens/mulher-adulta.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'imagens/idosa.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos um(a) ${gênero} com ${idade} anos de idade.`
+        res.appendChild(img)
+    }
+}
